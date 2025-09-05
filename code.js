82e432d79ef19b29301030f8257ee8bd0e150ef6
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-09-05 16:11:22 -0500

    updates to code

--- code.js
+++ code.js
@@ -1,5 +1,16 @@
-
+const fs = require('fs');
 
 function helloWorld() {
     console.log("Hello World");
+}
+
+const filePath = './data/productionLines.csv';
+readCSV(filePath);
+
+function readCSV(fp) {
+    const stream = fs.createReadStream(fp)
+    .pipe(csv())
+    .on('data', (row) => {
+        console.log(row);
+    });
 }
