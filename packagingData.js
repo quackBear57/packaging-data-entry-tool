832831454d15f2c1ccb74c284e2870c9916418f2
quackBear57 <89419185+quackBear57@users.noreply.github.com>
2025-09-08 15:37:52 -0500

    remove manager, add file upload function

--- packagingData.js
+++ packagingData.js
@@ -11,7 +11,18 @@ function readFile() {
 
     reader.addEventListener('load', () => {
         fileContent = reader.result;
-        console.log(fileContent);
+        contentArr = fileContent.split('\r\n'); // split on new line
+        var bottles = []; // empty array to hold all objects
+        var headers = contentArr[0].split(','); // set headers array
+        for(var i = 1; i < contentArr.length; i++) {
+            var data = contentArr[i].split(','); // split each line by comma
+            var obj = {};
+            for (var j = 0; j < data.length; j++) {
+                obj[headers[j]] = data[j];
+            }
+            bottles.push(obj);
+        }
+        console.log(bottles);
     },
     false,
     );
@@ -26,7 +37,7 @@ loadDataButton.addEventListener('click', () => {
     readFile();
 })
 
-export function bottleManager() {
+/* function bottleManager() {
     let bottles = [];
     
     function createBottle(name,
@@ -39,16 +50,24 @@ export function bottleManager() {
         height,
         heightPalletization
         ) {
-            const bottle = {};
-            bottle.name = name;
-            bottle.isActive = isActive;
-            bottle.lines = lines;
-            bottle.tpi = tpi;
-            bottle.weight = weight;
-            bottle.width = width;
-            bottle.depth = depth;
-            bottle.height = height;
-            bottle.heightPalletization = heightPalletization;
-            bottles.push(bottle);
-        }
+        
+        const bottle = {};
+        bottle.name = name;
+        bottle.isActive = isActive;
+        bottle.lines = lines;
+        bottle.tpi = tpi;
+        bottle.weight = weight;
+        bottle.width = width;
+        bottle.depth = depth;
+        bottle.height = height;
+        bottle.heightPalletization = heightPalletization;
+        bottles.push(bottle);
+    };
+
+    function getBottles() {
+        return [...bottles];
+    };
+
+    return { createBottle, getBottles};
 }
+ */
